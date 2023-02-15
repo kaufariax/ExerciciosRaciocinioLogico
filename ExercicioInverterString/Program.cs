22b956ae5155124bcca3877717b0ce5f98b21455
Kauane Farias <kauane.farias@wedecode.co>
2023-02-15 00:16:23 -0300

    @kaufariax [Task: Desafio de Inverter uma String] {15/02/2023}

---
+++ ExercicioInverterString/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExercicioInverterString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Deseja ver como fica um texto invertido? Digite-o aqui: ");
+            string? stringParaInverter = Console.ReadLine();
+            char[] caracteresDaString = stringParaInverter.ToCharArray();
+
+            for (int i = 0; i < stringParaInverter.Length / 2; i++)
+            {
+                char caracterAtual = caracteresDaString[i];
+                char caracterOposto = caracteresDaString[stringParaInverter.Length - i - 1];
+
+                caracteresDaString[i] = caracterOposto;
+                caracteresDaString[stringParaInverter.Length - i - 1] = caracterAtual;
+            }
+
+            Console.WriteLine(caracteresDaString);
+        }
+    }
+}
