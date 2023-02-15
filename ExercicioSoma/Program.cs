884678e4a673703d174ab9ef044bedbdcb31fa78
Kauane Farias <kauane.farias@wedecode.co>
2023-02-15 00:14:29 -0300

    @kaufariax [Task: Desafio de Soma] {15/02/2023}

---
+++ ExercicioSoma/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExercicioSoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice = 13, soma = 0, k = 0;
+
+            while (k < indice)
+            {
+                k = k + 1;
+                soma = soma + k;
+            };
+
+            Console.WriteLine($"Soma = {soma}");
+
+            // O resultado no console foi: Soma = 91
+        }
+    }
+}
